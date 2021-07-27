9d9503a4dcd3f9ed4eaaea4fc15580b19678dddd
Jacob Lerner <82839007+jakelerner-czi@users.noreply.github.com>
2021-07-27 13:19:26 -0400

    Credential Caching for the process-creds command (#158)
    * Initial thoughts on changed file structure
    
    * First working (returns valid looking result) version
    
    * add beeline to credProcessRun for timing check
    
    * remove some extraneous comments
    
    * Move creds process cache logic to pkg
    
    * change package name to match dir structure

--- cmd/creds-process.go
+++ cmd/creds-process.go
@@ -8,15 +8,18 @@ import (
 
 	"github.com/aws/aws-sdk-go/service/sts"
 	"github.com/chanzuckerberg/aws-oidc/pkg/aws_config_client"
+	"github.com/chanzuckerberg/aws-oidc/pkg/creds_process"
 	"github.com/chanzuckerberg/aws-oidc/pkg/getter"
 	oidc "github.com/chanzuckerberg/go-misc/oidc_cli"
 	oidc_client "github.com/chanzuckerberg/go-misc/oidc_cli/client"
+	"github.com/chanzuckerberg/go-misc/oidc_cli/storage"
+	"github.com/chanzuckerberg/go-misc/osutil"
+	"github.com/chanzuckerberg/go-misc/pidlock"
 	"github.com/honeycombio/beeline-go"
+	"github.com/mitchellh/go-homedir" // for storage, refactor out.
 	"github.com/pkg/errors"
 	"github.com/spf13/cobra"
 )
-
-const credProcessVersion = 1
 
 func init() {
 	credProcessCmd.Flags().StringVar(&clientID, "client-id", "", "CLIENT_ID generated from the OIDC application")
@@ -29,14 +32,6 @@ func init() {
 	rootCmd.AddCommand(credProcessCmd)
 }
 
-type credProcess struct {
-	Version         int    `json:"Version"`
-	AccessKeyID     string `json:"AccessKeyId"`
-	SecretAccessKey string `json:"SecretAccessKey"`
-	SessionToken    string `json:"SessionToken"`
-	Expiration      string `json:"Expiration"`
-}
-
 // credProcessCmd represents the cred-process command
 var credProcessCmd = &cobra.Command{
 	Use:   "creds-process",
@@ -46,36 +41,78 @@ var credProcessCmd = &cobra.Command{
 	RunE: credProcessRun,
 }
 
-func credProcessRun(cmd *cobra.Command, args []string) error {
-	ctx := cmd.Context()
+const (
+	lockFilePath          = "/tmp/aws-oidc-creds_process.lock"
+	defaultFileStorageDir = "~/.oidc-cli"
+	assumeRoleTime        = time.Hour // default to 1 hour
 
+)
+
+func updateCred(ctx context.Context,
+	awsOIDCConfig *aws_config_client.AWSOIDCConfiguration) (*creds_process.ProcessedCred, error) {
 	assumeRoleOutput, err := assumeRole(
 		ctx,
-		&aws_config_client.AWSOIDCConfiguration{
-			ClientID:  clientID,
-			IssuerURL: issuerURL,
-			RoleARN:   roleARN,
-		},
-		time.Hour, // default to 1 hour
+		awsOIDCConfig,
+		assumeRoleTime,
 	)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	creds := credProcess{
-		Version:         credProcessVersion,
+	creds := creds_process.ProcessedCred{
+		Version:         creds_process.ProcessedCredVersion,
 		AccessKeyID:     string(*assumeRoleOutput.Credentials.AccessKeyId),
 		SecretAccessKey: string(*assumeRoleOutput.Credentials.SecretAccessKey),
 		SessionToken:    string(*assumeRoleOutput.Credentials.SessionToken),
 		Expiration:      assumeRoleOutput.Credentials.Expiration.Format(time.RFC3339),
+		CacheExpiry:     *assumeRoleOutput.Credentials.Expiration,
+	}
+	return &creds, nil
+
+	output, err := json.MarshalIndent(creds, "", "	")
+	if err != nil {
+		return nil, errors.Wrap(err, "Unable to convert current credentials to json output") //error handling? as above
+	}
+	fmt.Println(string(output))
+
+	return nil, nil
+}
+
+func credProcessRun(cmd *cobra.Command, args []string) error {
+	ctx := cmd.Context()
+	ctx, span := beeline.StartSpan(ctx, "get_cred_process_run")
+	defer span.Send()
+
+	fileLock, err := pidlock.NewLock(lockFilePath)
+	if err != nil {
+		return errors.Wrap(err, "unable to create lock")
 	}
 
+	config := &aws_config_client.AWSOIDCConfiguration{
+		ClientID:  clientID,
+		IssuerURL: issuerURL,
+		RoleARN:   roleARN,
+	}
+
+	storage, err := getStorage(clientID, issuerURL)
+	if err != nil {
+		return err
+	}
+
+	cache := creds_process.NewCache(storage, updateCred, fileLock)
+
+	creds, err := cache.Read(ctx, config)
+	if err != nil {
+		return errors.Wrap(err, "Unable to process credentials.")
+	}
+	if creds == nil {
+		return errors.New("nil token from OIDC-IDP")
+	}
 	output, err := json.MarshalIndent(creds, "", "	")
 	if err != nil {
 		return errors.Wrap(err, "Unable to convert current credentials to json output")
 	}
 	fmt.Println(string(output))
-
 	return nil
 }
 
@@ -113,4 +150,33 @@ func getOIDCToken(
 		awsOIDCConfig.IssuerURL,
 		oidc_client.SetSuccessMessage(successMessage),
 	)
+}
+
+// TODO - Refactor out
+func getStorage(clientID string, issuerURL string) (storage.Storage, error) {
+	isWSL, err := osutil.IsWSL()
+	if err != nil {
+		return nil, err
+	}
+
+	// If WSL we use a file storage which does not cache refreshTokens
+	//    we do this because WSL doesn't have a graphical interface
+	//    and therefore limits how we can interact with a keyring (such as gnome-keyring).
+	// To limit the risks of having a long-lived refresh token around,
+	//    we disable this part of the flow for WSL. This could change in the future
+	//    when we find a better way to work with a WSL secure storage.
+	if isWSL {
+		return getFileStorage(clientID, issuerURL)
+	}
+
+	return storage.NewKeyring(clientID, issuerURL), nil
+}
+
+func getFileStorage(clientID string, issuerURL string) (storage.Storage, error) {
+	dir, err := homedir.Expand(defaultFileStorageDir)
+	if err != nil {
+		return nil, errors.Wrap(err, "could not expand path")
+	}
+
+	return storage.NewFile(dir, clientID, issuerURL), nil
 }
