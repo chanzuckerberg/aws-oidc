cd5e7730ec60092a12a33599cf10c51508f57ace
Allison Doami <adoami@chanzuckerberg.com>
2025-12-04 15:35:42 -0800

    flush-oidc-token-cache implementation

--- cmd/token.go
+++ cmd/token.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"os"
@@ -8,16 +9,19 @@ import (
 
 	"github.com/chanzuckerberg/go-misc/oidc/v4/cli"
 	"github.com/chanzuckerberg/go-misc/oidc/v4/cli/client"
+	"github.com/chanzuckerberg/go-misc/oidc_cli/oidc_impl/storage"
 
 	"github.com/spf13/cobra"
 )
 
 var deviceCodeFlow bool
+var flushOIDCTokenCache bool
 
 func init() {
 	tokenCmd.Flags().StringVar(&clientID, "client-id", "", "client_id generated from the OIDC application")
 	tokenCmd.Flags().StringVar(&issuerURL, "issuer-url", "", "The URL that hosts the OIDC identity provider")
 	tokenCmd.Flags().BoolVar(&deviceCodeFlow, "device-code-flow", false, "Use device code flow for authentication")
+	tokenCmd.Flags().BoolVar(&flushOIDCTokenCache, "flush-oidc-token-cache", false, "Flush the OIDC token cache")
 	tokenCmd.MarkFlagRequired("client-id")  // nolint:errcheck
 	tokenCmd.MarkFlagRequired("issuer-url") // nolint:errcheck
 
@@ -36,6 +40,20 @@ type stdoutToken struct {
 	Expiry      time.Time `json:"expiry,omitempty"`
 }
 
+func flushOIDCTokenCacheFn(ctx context.Context, clientID, issuerURL string) error {
+	storage, err := storage.GetOIDC(clientID, issuerURL)
+	if err != nil {
+		return fmt.Errorf("getting oidc token storage: %w", err)
+	}
+
+	err = storage.Delete(ctx)
+	if err != nil {
+		return fmt.Errorf("deleting token from storage: %w", err)
+	}
+
+	return nil
+}
+
 var tokenCmd = &cobra.Command{
 	Use:           "token",
 	Short:         "token prints the oidc tokens to stdout in json format",
@@ -49,6 +67,13 @@ var tokenCmd = &cobra.Command{
 
 		var token *client.Token
 		var err error
+		if flushOIDCTokenCache {
+			err = flushOIDCTokenCacheFn(ctx, clientID, issuerURL)
+			if err != nil {
+				return fmt.Errorf("flushing oidc token cache: %w", err)
+			}
+			return nil
+		}
 		if deviceCodeFlow {
 			token, err = cli.GetDeviceGrantToken(ctx, clientID, issuerURL, []string{"openid", "profile", "offline_access"})
 			if err != nil {
