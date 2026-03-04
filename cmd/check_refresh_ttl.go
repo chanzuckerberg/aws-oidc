d83ac4b692b9af1a639cdf270b5df96445996425
Jake Heath <jheath@chanzuckerberg.com>
2026-03-04 09:52:14 -0800

    feat: flag to make output of refresh ttl command machine readable

--- cmd/check_refresh_ttl.go
+++ cmd/check_refresh_ttl.go
@@ -9,9 +9,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
+const flagOutputDurationInHours = "output-duration-in-hours"
+
 func init() {
 	checkRefreshTTLCmd.Flags().StringVar(&clientID, "client-id", "", "client_id generated from the OIDC application")
 	checkRefreshTTLCmd.Flags().StringVar(&issuerURL, "issuer-url", "", "The URL that hosts the OIDC identity provider")
+	checkRefreshTTLCmd.Flags().Bool(flagOutputDurationInHours, false, "Output the TTL as a decimal number of hours (machine-readable)")
 	checkRefreshTTLCmd.MarkFlagRequired("client-id")  //nolint:errcheck
 	checkRefreshTTLCmd.MarkFlagRequired("issuer-url") //nolint:errcheck
 
@@ -36,6 +39,11 @@ var checkRefreshTTLCmd = &cobra.Command{
 	RunE: func(cmd *cobra.Command, args []string) error {
 		ctx := cmd.Context()
 
+		outputInHours, err := cmd.Flags().GetBool(flagOutputDurationInHours)
+		if err != nil {
+			return fmt.Errorf("reading %s flag: %w", flagOutputDurationInHours, err)
+		}
+
 		path := getNodeLocalCachePath(nodeLocalCache)
 		ttl, err := cli.CheckRefreshTokenTTL(
 			ctx,
@@ -47,7 +55,11 @@ var checkRefreshTTLCmd = &cobra.Command{
 			return fmt.Errorf("checking refresh token TTL: %w", err)
 		}
 
-		fmt.Fprintln(os.Stdout, ttl.String())
+		if outputInHours {
+			fmt.Fprintf(os.Stdout, "%.2f\n", ttl.Hours())
+		} else {
+			fmt.Fprintln(os.Stdout, ttl.String())
+		}
 		return nil
 	},
 }
